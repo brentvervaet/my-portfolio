ecffc9f2e06d129c8f90914c80ff1160e0956d23
Brent Vervaet <brent.vervaet@student.hogent.be>
2025-07-01 23:07:43 +0200

    refactor: comment out decorative gradient orbs in Page component

--- portfolio/src/app/about/page.tsx
+++ portfolio/src/app/about/page.tsx
@@ -5,8 +5,10 @@ import { motion } from 'framer-motion';
 const Page = () => {
   return (
     <main className="relative mx-auto max-w-3xl p-10">
-      <div className="absolute -top-20 -left-20 h-72 w-72 rounded-full bg-blue-400/20 blur-3xl"></div>
-      <div className="absolute -right-20 -bottom-20 h-72 w-72 rounded-full bg-purple-400/20 blur-3xl"></div>
+      {/*Decorative blurred gradient orbs in background*/}
+      {/*TODO*/}
+      {/*<div className="absolute -top-20 -left-20 h-72 w-72 rounded-full bg-blue-400/20 blur-3xl"></div>*/}
+      {/*<div className="absolute -right-20 -bottom-20 h-72 w-72 rounded-full bg-purple-400/20 blur-3xl"></div>*/}
 
       <div className="container mx-auto">
         <div className="mx-auto w-full max-w-4xl">
